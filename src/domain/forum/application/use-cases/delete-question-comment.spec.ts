8fddc7039333bab728456c61f47ee07b328a79d7
danilocucharro <danicucharro@outlook.com>
2026-04-29 11:15:33 -0300

    feat: Implementing a fature to delete a question comment & unit tests for it

---
+++ src/domain/forum/application/use-cases/delete-question-comment.spec.ts
@@ -0,0 +1,43 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { InMemoryQuestionCommentsRepository } from "../../../../../test/repositories/in-memory-question-comments-repository.js";
+import { makeQuestion } from "../../../../../test/factory/make-question.js";
+import { DeleteQuestionCommentUseCase } from "./delete-question-comment.js";
+import { makeQuestionComment } from "../../../../../test/factory/make-question-comment.js";
+import { UniqueEntityId } from "../../../../core/entities/unique-entity-id.js";
+
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: DeleteQuestionCommentUseCase
+
+describe('Delete Question Comment', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository = new InMemoryQuestionCommentsRepository()
+
+    sut = new DeleteQuestionCommentUseCase(inMemoryQuestionCommentsRepository)
+  })
+
+  it('should be able to delete a question comment', async () => {
+    const newQuestionComment = makeQuestionComment()
+    await inMemoryQuestionCommentsRepository.create(newQuestionComment)
+
+    await sut.execute({
+      authorId: newQuestionComment.authorId.toString(),
+      questionCommentId: newQuestionComment.id.toString()
+    })
+
+    expect(inMemoryQuestionCommentsRepository.items).toHaveLength(0)
+  })
+
+  it('should NOT be able to delete another question comment', async () => {
+    const newQuestionComment = makeQuestionComment({
+      authorId: new UniqueEntityId('author-1')
+    })
+    await inMemoryQuestionCommentsRepository.create(newQuestionComment)
+
+    expect(() => {
+      return sut.execute({
+        authorId: 'author-2',
+        questionCommentId: newQuestionComment.id.toString()
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
